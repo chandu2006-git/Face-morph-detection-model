a349f3e5f43840fea5effa52ac74009362eb7dc5
chandu2006-git <akulachandrasekhar12323@gmail.com>
2026-05-24 14:52:42 +0530

    Final fix - remove app.run and simplify root route

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,16 @@
 from flask import Flask, render_template
 from backend.routes.predict import predict_bp
-from flask import Flask, render_template
 from flask_cors import CORS
-
 
 app = Flask(__name__, 
             template_folder="../templates", 
             static_folder="../static")
+
 CORS(app)
+
 # Register API
 app.register_blueprint(predict_bp)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-    #  http://127.0.0.1:5000
+    return "MorphGuard API is running 🚀"
