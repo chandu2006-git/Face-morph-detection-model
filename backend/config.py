01c6e3dde5378757c9651c2f05e2b81aab540b37
chandu2006-git <akulachandrasekhar12323@gmail.com>
2026-05-25 10:13:07 +0530

    indidn

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,12 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# 🔥 MODEL FILE LOCATION (inside backend/model/)
 MODEL_PATH = os.path.join(BASE_DIR, "model", "FINAL_WORKING_MODEL.h5")
+
+# 🔥 DRIVE DOWNLOAD LINK
 MODEL_URL = "https://drive.google.com/uc?id=1GgDJt8PVoW4QQPRJ7xwm1Ry0aLs5bAa4"
+
+# 🔥 IMAGE SETTINGS
 IMAGE_SIZE = (299, 299)
 THRESHOLD = 0.5
